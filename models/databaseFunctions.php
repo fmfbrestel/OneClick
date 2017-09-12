793e390e720921ea82dc3bf0db39f5c1f23e4278
mb236341 <mb236341@LR00444.southeast.edu>
2017-09-12 12:49:27 -0500

    more db functions

--- models/databaseFunctions.php
+++ models/databaseFunctions.php
@@ -7,10 +7,41 @@
         $statement = $db->prepare($query);
         $statement->bindValue(':user', $userName);
         $statement->execute();
-        $userInfo = $statement->fetchAll();
+        $userInfo = $statement->fetch();
         $statement->closeCursor();
 
         return $userInfo;
+    }
+    
+    function get_purchase_relation($prodID){
+        global $db;
+        $query = 'SELECT * from PurchRelate WHERE prodID=:prodID';
+        //prepare the query, bind the values, then you execute
+        $statement = $db->prepare($query);
+        $statement->bindValue(':prodID', $prodID);
+        $statement->execute();
+        $purchRelate = $statement->fetch();
+        $statement->closeCursor();
+
+        return $purchRelate;
+    }
+    
+    function get_view_relation($prodID){
+        global $db;
+        $query = 'UPDATE * from PurchRelate WHERE prodID=:prodID';
+        //prepare the query, bind the values, then you execute
+        $statement = $db->prepare($query);
+        $statement->bindValue(':prodID', $prodID);
+        $statement->execute();
+        $viewRelate = $statement->fetch();
+        $statement->closeCursor();
+
+        return $viewRelate;
+    }
+    
+    function update_purchase_relation($prodID){
+        global $db;
+        
     }
 
 ?>
